63784eafd52622cdf4d9030cc323db3528f307b3
andretran <Andre.n.tran@gmail.com>
2014-07-23 10:48:58 -0700

    starting heir_bnb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HeirBnb::Application.config.secret_key_base = '4e7d27e321f7701b7b80403c2fcf8be0413034c31baeaee1b992a80d8c6eda9f4a61551ebfd323beaf3eaa4be838da5ef6e9eb9e5aad4014b910cc9d656e0817'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HeirBnb::Application.config.session_store :cookie_store, key: '_heir_bnb_session'
